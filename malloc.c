2cb4c806dd0863a7dc78ecb38e3e7a61c23ace18
cybernuki <1164@holbertonschool.com>
2021-01-27 01:03:34 +0000

    malloc

--- malloc.c
+++ malloc.c
@@ -1,73 +1,144 @@
 #include "malloc.h"
 
-/**
- * _malloc_sbrk - extend the program break to allocate memory
- *
- * @needed: number of bytes needed for requested memory, padding and header
- * @chunk: pointer to the start of the unused chunk
- * @unused: number of unused allocated bytes
- *
- * Return: If memory allocation fails, return NULL.
- * Otherwise, return a pointer to the start of the allocated memory region.
- */
-static void *_malloc_sbrk(size_t needed, void *chunk, size_t unused)
-{
-	chunk_header_t *header = chunk;
-	long page_size = sysconf(_SC_PAGESIZE);
-	size_t n_pages =
-		((needed + sizeof(*header) - unused - 1) / page_size) + 1;
+static heap_info heap;
 
-	errno = 0;
-	if (sbrk(page_size * n_pages) == (void *)-1 && errno)
+/**
+ * print_heap - prints contents of heap, used for visualization
+ */
+void print_heap(void)
+{
+	char *p = heap.heap_start;
+	size_t i = 0;
+
+	if (!p)
+		return;
+	while (GET_SIZE(p) >= HDR_SZ + MIN_SIZE)
 	{
-		error(0, errno, NULL);
-		return (NULL);
+		p += GET_SIZE(p);
+		++i;
 	}
-	CHUNK_SET_PREV(header, 0);
-	CHUNK_SET_SIZE(header, page_size * n_pages + unused);
-	CHUNK_SET_FREE(header);
-	return (chunk);
+	if (p + HDR_SZ != sbrk(0))
+		exit(1);
 }
 
 /**
- * _malloc - dynamically allocate heap memory
- *
- * @size: number of bytes to allocate
- *
- * Return: If memory allocation fails, return NULL.
- * Otherwise, return a pointer to the start of the allocated memory region.
+ * add_header - add header info
+ * @addr: header address
+ * @size: size of this memory block, size + HDR_SZ
+ * @prev: previous chunk offset, 0 if previous is allocated
  */
-void *_malloc(size_t size)
+void add_header(void *addr, size_t size, size_t prev)
 {
-	static void *start;
-	void *chunk = start;
-	chunk_header_t *header = chunk;
-	size_t needed = size + PADDING(size) + sizeof(*header);
-	size_t unused = 0;
+	block_info *block;
 
-	if (start)
+	block = addr;
+	block->size = prev ? size + HDR_SZ + 1 : size + HDR_SZ;
+	block->prev = prev;
+}
+
+/**
+ * expand - expand heap by calling sbrk()
+ * @size: size needed by user
+ * Return: pointer to memory block header
+ */
+void *expand(size_t size)
+{
+	size_t pg_sz;
+	char *p, *tmp;
+
+	pg_sz = 2 * HDR_SZ + MIN_SIZE + size;
+	pg_sz += heap.heap_start ? 0 : HDR_SZ;
+	pg_sz = align_up(pg_sz, PAGESIZE);
+	p = sbrk(pg_sz);
+	if (p == (void *)-1 && errno == ENOMEM)
+		return (NULL);
+	if (heap.heap_start == NULL)
 	{
-		while (CHUNK_GET_USED(header))
-			header = chunk =
-				(char *)chunk + CHUNK_GET_SIZE(header);
-		unused = CHUNK_GET_SIZE(header);
+		/* Calling expand() for the first time */
+		heap.heap_start = p;
+		/* Set the first chunk, returned to USER */
+		add_header(p, size, 0);
+		/* Set middle and sentinel chunks */
+		add_header(p + GET_SIZE(p), pg_sz - size - 3 * HDR_SZ, 0);
+		add_header(p + pg_sz - HDR_SZ, 0, pg_sz - size - 2 * HDR_SZ);
+		return (p);
 	}
 	else
 	{
-		chunk = start = sbrk(0);
+		/*
+		 * Subsequent calls to expand()
+		 * Make p point to sentinel chunk (block) and convert
+		 * that sentinel to regular chunk, which will be returned to
+		 * a user
+		 */
+		p -= HDR_SZ;
+		tmp = p;
+		/* Change previous sentinel chunk, returned to USER */
+		/* FIXME: cause GET_PREV is not 0, size is size+1*/
+		add_header(p, size, GET_PREV(p));
+		p += GET_SIZE(p);
+		add_header(p, pg_sz - size - 2 * HDR_SZ, 0);
+		add_header(p + GET_SIZE(p), 0, pg_sz - size - HDR_SZ);
+		return (tmp);
 	}
-	if (unused < needed + sizeof(*header))
-	{
-		header = chunk = _malloc_sbrk(needed, chunk, unused);
-		unused = CHUNK_GET_SIZE(header);
-	}
-	if (!chunk)
+}
+
+/**
+ * find_block - find an unused memory block to return to a user
+ * @size: size needed by a user
+ * Return: pointer to a memory block header
+ */
+void *find_block(size_t size)
+{
+	char *p = heap.heap_start, *tmp;
+	size_t req_sz = size + HDR_SZ;
+
+	if (!p)
 		return (NULL);
-	CHUNK_SET_SIZE(header, needed);
-	CHUNK_SET_USED(header);
-	header = (chunk_header_t *)((char *)chunk + needed);
-	CHUNK_SET_PREV(header, 0);
-	CHUNK_SET_SIZE(header, unused - needed);
-	CHUNK_SET_FREE(header);
-	return ((char *)chunk + sizeof(*header));
+
+	while (GET_SIZE(p) >= HDR_SZ + MIN_SIZE)
+	{
+		p += GET_SIZE(p);
+		if ((_GET_SIZE(p) & 1) && GET_SIZE(p - GET_PREV(p)) >= req_sz)
+		{
+			tmp = p - GET_PREV(p);
+			if (GET_SIZE(tmp) >= req_sz + HDR_SZ + MIN_SIZE)
+			{
+				/* Split the chunk */
+				add_header(tmp + req_sz, GET_SIZE(tmp) - req_sz - HDR_SZ, 0);
+				((block_info *)tmp)->size = req_sz;
+				((block_info *)p)->prev = GET_SIZE(tmp + req_sz);
+			}
+			else
+			{
+				/* Don't split the chunk */
+				((block_info *)p)->size &= LSB_ZERO_MASK;
+				((block_info *)p)->prev = 0;
+			}
+
+			return (tmp);
+		}
+	}
+	return (NULL);
+}
+
+/**
+ * _malloc - malloc function implementation
+ * @size: size need by a user
+ * Return: a pointer to heap with at least @size usable memory,
+ * NULL on failure or when @size is 0
+ */
+void *_malloc(size_t size)
+{
+	char *p;
+
+	size = align_up(size, ALIGNMENT);
+	if (size == 0)
+		return (NULL);
+	p = find_block(size);
+	if (!p)
+		p = expand(size);
+	if (!p)
+		return (NULL);
+	return (p + HDR_SZ);
 }
