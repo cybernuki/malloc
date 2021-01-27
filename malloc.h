c11730fe277e5916a223f551e652dcda5bf670d9
cybernuki <1164@holbertonschool.com>
2021-01-27 00:49:10 +0000

    naive implementation

---
+++ malloc.h
@@ -0,0 +1,50 @@
+#ifndef _MALLOC_H_
+#define _MALLOC_H_
+
+#include <stddef.h>
+
+#define FOOT_SIZE (sizeof(size_t))
+#define HEAD_SIZE (sizeof(size_t))
+
+#define MIN_SIZE (FOOT_SIZE + HEAD_SIZE)
+#define TOP_SIZE MIN_SIZE
+
+#define ALIGNMENT (sizeof(void *))
+#define PADDING(n) ((ALIGNMENT - ((n) & (ALIGNMENT - 1))) & (ALIGNMENT - 1))
+#define ALIGNED(n) ((n) + PADDING(n))
+#define NEEDED(n) (MIN_SIZE + ALIGNED(n))
+
+/**
+ * struct chunk_s - malloc chunk header
+ *
+ * @prev_size: size of the previous chunk (only if not in-use)
+ * @size: size of the chunk and in-use bit of the previous chunk
+ * @next_free: pointer to the next free chunk in a circular doubly-linked list
+ * @prev_free: pointer to the prev free chunk in a circular doubly-linked list
+ */
+typedef struct chunk_s
+{
+    size_t prev_size;
+    size_t size;
+    struct chunk_s *next_free;
+    struct chunk_s *prev_free;
+} chunk_t;
+
+#define CHUNK_PREV_USED_MASK ((size_t)1)
+#define CHUNK_PREV_SIZE(p) ((p)->prev_size)
+#define CHUNK_SIZE(p) ((p)->size & ~CHUNK_PREV_USED_MASK)
+#define CHUNK_PREV_USED(p) ((p)->size & CHUNK_PREV_USED_MASK)
+#define CHUNK_NEXT(p) ((chunk_t *)((char *)(p) + CHUNK_SIZE(p)))
+#define CHUNK_PREV(p) ((chunk_t *)((char *)(p)-CHUNK_PREV_SIZE(p)))
+#define CHUNK_SET_PREV_SIZE(p, n) ((p)->prev_size = (n))
+#define CHUNK_SET_SIZE(p, n) ((p)->size = (n) | CHUNK_PREV_USED(p))
+#define CHUNK_SET_PREV_USED(p) ((p)->size |= CHUNK_PREV_USED_MASK)
+#define CHUNK_UNSET_PREV_USED(p) ((p)->size &= ~CHUNK_PREV_USED_MASK)
+
+void *naive_malloc(size_t size);
+void *_malloc(size_t size);
+void *_calloc(size_t nmemb, size_t size);
+void *_realloc(void *ptr, size_t size);
+void _free(void *ptr);
+
+#endif /* _MALLOC_H_ */
---
+++ naive_malloc.c
@@ -0,0 +1,84 @@
+#include "malloc.h"
+
+/**
+ * set_hdr - set chunk headers
+ * @chunk: pointer to start of chunk
+ * @chunk_size: size of chunk
+ * @excess_mem: pointer to variable tracking excess memory
+ */
+void set_hdr(char *chunk, size_t chunk_size, size_t *excess_mem)
+{
+    *excess_mem -= chunk_size;
+    *(size_t *)chunk = chunk_size;
+    *(size_t *)(chunk + *(size_t *)chunk) = *excess_mem;
+}
+
+/**
+ * find_unused - find unused chunk block
+ * @heap_start: pointer to heap start
+ * @call_nb: specifies how many times malloc has been called
+ *
+ * Return: pointer to start of unused chunk
+ */
+void *find_unused(char *heap_start, size_t call_nb)
+{
+    while (call_nb > 0)
+    {
+        heap_start += *(size_t *)heap_start;
+        --call_nb;
+    }
+    return (heap_start);
+}
+
+/**
+ * extend - calls sbrk with size aligned to page size
+ * @chunk_size: aligned size request
+ * @excess_mem: pointer to variable tracking excess memory
+ *
+ * Return: pointer to start of new chunk, NULL on failure
+ */
+void *extend(size_t chunk_size, size_t *excess_mem)
+{
+    void *chunk;
+
+    chunk = sbrk(align(chunk_size, sysconf(_SC_PAGESIZE)));
+    if (chunk == (void *)-1)
+        return (NULL);
+    *excess_mem += align(chunk_size, sysconf(_SC_PAGESIZE));
+    return (chunk);
+}
+
+/**
+ * naive_malloc - naive malloc: dynamically allocates memory to the heap
+ * @size: number of bytes to allocate
+ *
+ * Return: the memory address newly allocated, or NULL on error
+ */
+void *naive_malloc(size_t size)
+{
+    void *chunk;
+    static void *heap_start;
+    static size_t call_nb;
+    size_t hdr_size, chunk_size, excess_mem;
+
+    hdr_size = sizeof(size_t);
+    chunk_size = align(size, sizeof(void *)) + hdr_size;
+    if (!heap_start)
+    {
+        excess_mem = 0;
+        heap_start = chunk = extend(chunk_size, &excess_mem);
+        if (!chunk)
+            return (NULL);
+    }
+    else
+    {
+        chunk = find_unused(heap_start, call_nb);
+        excess_mem = *(size_t *)chunk;
+        if (excess_mem < chunk_size + hdr_size)
+            if (!extend(chunk_size, &excess_mem))
+                return (NULL);
+    }
+    set_hdr(chunk, chunk_size, &excess_mem);
+    ++call_nb;
+    return ((char *)chunk + hdr_size);
+}
