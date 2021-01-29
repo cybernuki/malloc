efe2b1312ec321ecc9094db7adf8ed99e9902c31
cybernuki <1164@holbertonschool.com>
2021-01-29 21:15:28 +0000

    Fix

--- malloc.h
+++ malloc.h
@@ -63,7 +63,6 @@ typedef struct chunk_s
 void *naive_malloc(size_t size);
 void *_malloc(size_t size);
 void *_calloc(size_t nmemb, size_t size);
-void *_realloc(void *ptr, size_t size);
 void _free(void *ptr);
 
 #endif /* _MALLOC_H_ */
