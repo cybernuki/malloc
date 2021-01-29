ee2e5dd5887b54f1060ca770d2551c46a427c385
cybernuki <1164@holbertonschool.com>
2021-01-29 21:15:43 +0000

    fix

--- calloc.c
+++ calloc.c
@@ -8,13 +8,13 @@
 */
 void *_calloc(size_t nmemb, size_t size)
 {
-    size_t required_size = 0;
-    void *index = NULL;
+	size_t required_size = 0;
+	void *index = NULL;
 
-    required_size = nmemb * size;
-    index = _malloc(required_size);
-    if (!index)
-        return (NULL);
-    memset(index, 0, required_size);
-    return (index);
+	required_size = nmemb * size;
+	index = _malloc(required_size);
+	if (!index)
+		return (NULL);
+	memset(index, 0, required_size);
+	return (index);
 }
